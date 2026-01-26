96dbc826c30d7e33b69a9a807b9a62669cb95ce8
browleeoh <bsalhuan@fau.edu>
2026-01-25 22:35:41 -0500

    init: otsukaresamadeshita

--- app/db/db.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import * as schema from "./schema";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set in environment variables");
-}
-
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
--- app/features/video-editor/constants.ts
+++
@@ -1,3 +0,0 @@
-export const FINAL_VIDEO_PADDING = 0.5 - 0.08;
-export const INSERTION_POINT_ID = "insertion-point";
-export const BEAT_DURATION = 0.4;
